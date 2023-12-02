89f978074f1af90ea986af67c2bbafeb721a70ff
Ayagi3678 <114006559+Ayagi3678@users.noreply.github.com>
2023-12-03 07:36:18 +0900

    いらんusing消す

--- Assets/MMMCFeedbacks/Runtime/FeedbackPlayerUtility.cs
+++ Assets/MMMCFeedbacks/Runtime/FeedbackPlayerUtility.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using MagicTween;
-using MagicTween.Experimental;
 
 namespace MMMCFeedbacks.Core
 {
